232f1a76c91efebfc9c74dd603615f3ce62b58bf
cgcgbcbc <cgcgbcbc@163.com>
2015-05-25 22:59:19 +0800

    add test for checkin on ticket twice at same time

--- test/routes/checkin.js
+++ test/routes/checkin.js
@@ -72,6 +72,96 @@ describe('test routes/checkin', function() {
                     })
                     .end(done);
             });
+            it('should return used if valid checkin with used ticket', function(done) {
+                agent
+                    .post(baseUrl + '?actid=5562e19b61fe7b8f24a70b3a')
+                    .send({uid: '2014311933'})
+                    .expect(200)
+                    .expect(function(res) {
+                        should(res.body.result).be.a.String.and.eql('error');
+                        should(res.body.msg).be.a.String.and.eql('used');
+                    })
+                    .end(done);
+            });
+            it('should return used if valid checkin with used ticket', function(done) {
+                agent
+                    .post(baseUrl + '?actid=5562e19b61fe7b8f24a70b3a')
+                    .send({uid: 'ylvmhEij5tZkwRnpm4DFzIQvxueI8WGw'})
+                    .expect(200)
+                    .expect(function(res) {
+                        should(res.body.result).be.a.String.and.eql('error');
+                        should(res.body.msg).be.a.String.and.eql('used');
+                    })
+                    .end(done);
+            });
+            it('should return used if no ticket', function(done) {
+                agent
+                    .post(baseUrl + '?actid=5562e19b61fe7b8f24a70b3a')
+                    .send({uid: '2014311900'})
+                    .expect(200)
+                    .expect(function(res) {
+                        should(res.body.result).be.a.String.and.eql('error');
+                        should(res.body.msg).be.a.String.and.eql('noticket');
+                    })
+                    .end(done);
+            });
+            it('should return noticket if no ticket', function(done) {
+                agent
+                    .post(baseUrl + '?actid=5562e19b61fe7b8f24a70b3a')
+                    .send({uid: 'ffffffffffffffffffffffffffffffff'})
+                    .expect(200)
+                    .expect(function(res) {
+                        should(res.body.result).be.a.String.and.eql('error');
+                        should(res.body.msg).be.a.String.and.eql('noticket');
+                    })
+                    .end(done);
+            });
+            it('should return rejected if use other activity ticket', function(done) {
+                agent
+                    .post(baseUrl + '?actid=5562e19b61fe7b8f24a70b3a')
+                    .send({uid: 'wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww'})
+                    .expect(200)
+                    .expect(function(res) {
+                        should(res.body.result).be.a.String.and.eql('error');
+                        should(res.body.msg).be.a.String.and.eql('rejected');
+                    })
+                    .end(done);
+            });
+            it('should reject the same ticket checkin at second time', function(done) {
+                var events = require('events');
+                var doneWrapper = new events.EventEmitter();
+                doneWrapper.done = function(res) {
+                    console.log(res);
+                    doneWrapper.emit('done', res);
+                };
+                doneWrapper.doneCount = 0;
+                doneWrapper.on('done', function(res) {
+                    this.doneCount ++;
+                    if (this.doneCount === 1) {
+                        should(res.msg).be.a.String.and.eql('accepted');
+                    }
+                    else if (this.doneCount === 2) {
+                        should(res.result).be.a.String.and.eql('error');
+                        done();
+                    }
+                });
+                agent
+                    .post(baseUrl + '?actid=5562e19b61fe7b8f24a70b3a')
+                    .send({uid: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'})
+                    .expect(200)
+                    .end(function(err, res) {
+                        if (err) throw err;
+                        doneWrapper.done(res.body);
+                    });
+                agent
+                    .post(baseUrl + '?actid=5562e19b61fe7b8f24a70b3a')
+                    .send({uid: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'})
+                    .expect(200)
+                    .end(function(err, res) {
+                        if (err) throw err;
+                        doneWrapper.done(res.body);
+                    });
+            });
         });
     });
 });
