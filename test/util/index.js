e7b4ce491bb65b1c3b46799d17746125a4099b95
cgcgbcbc <cgcgbcbc@163.com>
2015-05-28 11:21:15 +0800

    add clear database method

--- test/util/index.js
+++ test/util/index.js
@@ -5,9 +5,14 @@ var models = require('../../models/models')
     , db = models.db;
 
 exports = module.exports;
-exports.loadFixture = loadFixture;
+exports.loadFixture = loadFixtadd;
+exports.clearData = clearData;
 
 
 function loadFixture(collection, data, callback) {
     db[collection].insert(data, callback);
+}
+
+function clearData(callback) {
+    db.dropDatabase(callback);
 }
